e53517aa131bd09d988fd41d55055f9dc180913e
Tenkouu <anhbayr.922@gmail.com>
2021-10-22 00:22:54 +0800

    41.

--- schemas/schema.js
+++ schemas/schema.js
@@ -7,10 +7,29 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      title: "Постууд",
+      name: "post",
+      type: "document",
+      fields: [
+        {
+          title: "Постын гарчиг",
+          name: "title",
+          type: "string",
+          description:
+            "Блогийн постын үндсэн гарчиг, 50 тэмдэгтээс бүү хэтрүүлээрэй",
+        },
+        {
+          title: "Дэд гарчиг",
+          name: "subtitle",
+          type: "string",
+          description: "Постын төрлийг текстээр бичих",
+        },
+      ],
+    },
   ]),
-})
+});
